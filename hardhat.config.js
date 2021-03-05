8510836f1bc333d90b3b31a8ba3ee3c90fc1622d
oliviel <olivielultimate@gmail.com>
2021-03-05 17:38:22 -0400

    Update the readme added the env.example file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,13 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-
+require('dotenv').config();
 require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+      },
+    }
+  },
   solidity: "0.7.3",
 };
