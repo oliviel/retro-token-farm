cac3a48639a579ce6d1ae137c97f0e7c703c280e
oliviel <olivielultimate@gmail.com>
2021-03-07 18:22:36 -0400

    Starter project

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,12 +2,5 @@ require('dotenv').config();
 require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
-  networks: {
-    hardhat: {
-      accounts: {
-        mnemonic: process.env.MNEMONIC,
-      },
-    }
-  },
   solidity: "0.7.3",
 };
--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import { useEffect, useState } from 'react';
-import { ethers } from 'ethers';
 import {
   NoWalletDetected, 
   ConnectWallet, 
@@ -9,185 +7,17 @@ import {
   Form,
   Balances
 } from './components';
-import { parseEther } from './utils';
-import RetroTokenContract from './abis/RetroToken.json';
-import retroTokenAddress from './abis/retrotoken-address.json';
-import DaiTokenContract from './abis/DaiToken.json';
-import daiTokenAddress from './abis/daitoken-address.json';
-import TokenFarmContract from './abis/TokenFarm.json';
-import tokenFarmAddress from './abis/tokenfarm-address.json';
 
 function App() {
-  const [userAddress, setUserAddress] = useState('');
-  const [isLoading, setIsloading] = useState(true);
-  const [amount, setAmount] = useState('');
-
-  const [retroTokenContract, setRetroTokenContract] = useState({});
-  const [daiTokenContract, setDaiTokenContract] = useState({});
-  const [tokenFarmContract, setTokenFarmContract] = useState({});
-
-  const [balance, setBalance] = useState({
-    retroToken: '0',
-    daiToken: '0',
-    staking: '0',
-  });  
-
-  useEffect(() => {
-    async function fetchData() {
-      await loadBlockchainData(); 
-    }
-    fetchData();
-  }, []);
-
-  async function loadBlockchainData() {    
-    try {
-      setIsloading(true);
-
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-      const signer = provider.getSigner();
-
-      const accountAddress = await signer.getAddress();
-
-      const retroToken = new ethers.Contract(
-        retroTokenAddress.RetroToken, 
-        RetroTokenContract.abi, 
-        signer
-      );
-
-      const daiToken = new ethers.Contract(
-        daiTokenAddress.DaiToken,
-        DaiTokenContract.abi,
-        signer
-      )
-
-      const tokenFarm = new ethers.Contract(
-        tokenFarmAddress.TokenFarm,
-        TokenFarmContract.abi,
-        signer
-      )
-
-      const daiBalance = await daiToken.balanceOf(accountAddress);
-
-      const retroBalance = await retroToken.balanceOf(accountAddress);
-
-      const tokenFarmBalance = await tokenFarm.stakingBalance(accountAddress);
-
-      setUserAddress(accountAddress);    
-      setRetroTokenContract(retroToken);
-      setDaiTokenContract(daiToken);
-      setTokenFarmContract(tokenFarm);
-      setBalance({
-        retroToken: retroBalance.toString(),
-        daiToken: daiBalance.toString(),
-        staking: tokenFarmBalance.toString(),
-      })
-    } catch (error) {
-      console.log(error)
-    } finally {
-      setIsloading(false);
-    }
-  }
-
-  async function stakeTokens() {
-    try {
-      setIsloading(true);
-      
-      const approve = await daiTokenContract.approve(
-        tokenFarmContract.address, 
-        parseEther(amount)
-      );
-
-      const response = await approve.wait();
-
-      const tx = await tokenFarmContract.stakeTokens(parseEther(amount));
-
-      const receipt = await tx.wait();
-
-      if (receipt.status === 0 || response.status === 0) {
-        throw new Error("Transaction failed");
-      }
-      
-      const daiBalance = await daiTokenContract.balanceOf(userAddress);
-      const tokenFarmBalance = await tokenFarmContract.stakingBalance(userAddress);
-
-      setBalance((prevBalance) => ({ 
-        ...prevBalance,
-        daiToken: daiBalance.toString(),
-        staking: tokenFarmBalance.toString(),
-      }));
-    } catch (error) {
-      console.log('EROROR', error);
-    } finally {
-      setAmount('');
-      setIsloading(false);
-    }
-  }
-  
-  async function unstakeTokens() {
-    try {
-      setIsloading(true);
-      const tx = await tokenFarmContract.unstakeTokens();
-      const receipt = await tx.wait();
-
-      if (receipt.status === 0) {
-        throw new Error("Transaction failed");
-      }
-
-      const daiBalance = await daiTokenContract.balanceOf(userAddress);
-      const tokenFarmBalance = await tokenFarmContract.stakingBalance(userAddress);
-
-      setBalance((prevBalance) => ({ 
-        ...prevBalance,
-        daiToken: daiBalance.toString(),
-        staking: tokenFarmBalance.toString(),
-      }));
-    } catch (error) {
-      console.log('ERROR', error);
-    } finally {
-      setIsloading(false);
-    }
-  }
-
-  async function triggerWallet() {
-    await window.ethereum.enable();
-  }
-
-  function getRandomNumber(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  if (window.ethereum === undefined) {
-    return <NoWalletDetected />;
-  }
-
-  if (isLoading) {
-    return <Loading loadingPercent={getRandomNumber(1, 100)} />;
-  }
-
-  if (!userAddress) {
-    return <ConnectWallet connectWallet={triggerWallet} />;
-  }
- 
   return (
     <>
       <Header />
       <Container className="mt-5">
         <Balances
-          stakingBalance={balance.staking}
-          retroTokenBalance={balance.retroToken}
         />
       </Container>
       <Container>
-        <Form 
-          daitokenBalance={balance.daiToken} 
-          amount={amount}
-          handleAmount={setAmount}
-          stakeTokens={stakeTokens}
-          unstakeTokens={unstakeTokens}
-        />
+        <Form />
       </Container>
     </>
   )
